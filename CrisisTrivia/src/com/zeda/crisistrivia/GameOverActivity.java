bbc5e9a1ad48d5123cdb3cc5951954875b6b6553
Gabo <Gabo@Portatalar>
2012-12-03 14:57:36 +0100

    Added GameOver screen

---
+++ CrisisTrivia/src/com/zeda/crisistrivia/GameOverActivity.java
@@ -0,0 +1,24 @@
+package com.zeda.crisistrivia;
+
+import com.zeda.crisistrivia.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+public class GameOverActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_game_over);
+		
+	}
+}
